bf836ede4effba126060b3431caaabbe900c453d
NeptoonsSweetylyx <134655478+NeptoonsSweetylyx@users.noreply.github.com>
2024-10-15 08:12:15 +0700

    Update UserBot.py

--- UserBot.py
+++ UserBot.py
@@ -87,9 +87,9 @@ async def promote(event):
         try:
             if reply_message.media:
                 media_path = await client.download_media(reply_message.media)
-                await client.send_file(dialog.id, media_path, caption=append_watermark_to_message(reply_message.message, parse_mode: "HTML"))
+                await client.send_file(dialog.id, media_path, caption=append_watermark_to_message(reply_message.message, parse_mode: 'html'))
             else:
-                message_with_watermark = append_watermark_to_message(reply_message.message, parse_mode: "HTML")
+                message_with_watermark = append_watermark_to_message(reply_message.message, parse_mode: 'html')
                 await client.send_message(dialog.id, message_with_watermark)
             sent_count += 1
             progress = (sent_count / total_groups) * 100
